a089fb7d042b9b2f38e4d0b1f3af3b0e8096655b
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-02-17 22:35:56 +0100

    implemented reports formatter

---
+++ src/main/java/com/insightprovider/f1/formatter/ReportFormatter.java
@@ -0,0 +1,43 @@
+package com.insightprovider.f1.formatter;
+
+import static java.lang.System.lineSeparator;
+import static java.util.Comparator.comparing;
+import static java.time.LocalTime.ofNanoOfDay;
+import static java.lang.String.format;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import com.insightprovider.f1.model.RaceResult;
+
+public class ReportFormatter {
+
+	public static final String HYPHEN_DELIMITER = "-";
+	public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("mm:ss.SSS");
+
+	public String build(List<RaceResult> raceResults, int bestRacersNumber) {
+		StringBuilder result = new StringBuilder();
+		int maxNameLength = getMaxFieldLength(raceResults, RaceResult::getFullName);
+		int maxTeamLength = getMaxFieldLength(raceResults, RaceResult::getTeam);
+		AtomicInteger number = new AtomicInteger();
+		String pattern = "%02d. %-" + maxNameLength + "s | %-" + maxTeamLength + "s | %s";
+		raceResults.stream().sorted(comparing(RaceResult::getLapTime)).forEach(s -> {
+			if ((number.get() == bestRacersNumber)) {
+				result.append(HYPHEN_DELIMITER.repeat(maxNameLength + maxTeamLength + 19)).append(lineSeparator());
+			}
+			result.append(format(pattern, number.incrementAndGet(), s.getFullName(), s.getTeam(),
+					formatToTime(s.getLapTime()))).append(lineSeparator());
+		});
+		return result.toString();
+	}
+
+	private int getMaxFieldLength(List<RaceResult> racers, Function<RaceResult, String> function) {
+		return racers.stream().map(function).mapToInt(String::length).max().orElse(0);
+	}
+
+	private String formatToTime(Duration duration) {
+		return ofNanoOfDay(duration.toNanos()).format(TIME_FORMATTER);
+	}
+	
+}
